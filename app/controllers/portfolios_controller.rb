675c998e6ae3f428907515b316738c21b3aa4944
Greg Cosgrove <gregcosgrove@gmail.com>
2018-07-16 14:24:42 -0400

    Add portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,31 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def portfolio_item_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 end
+
