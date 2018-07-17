48aaff3aeb02fbceaa282c5f02c5eef9eb72c3ca
Greg Cosgrove <gregcosgrove@gmail.com>
2018-07-16 20:37:06 -0400

    Add custom scopes for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,7 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+  scope :react_js, -> { where(subtitle: "React.js") }
+  scope :node_js, -> { where(subtitle: "Node.js") }
 end
