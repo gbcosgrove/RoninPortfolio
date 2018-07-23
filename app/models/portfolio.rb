78451402e150d4275fa6a0a9bcec3e1b55cb0660
Greg Cosgrove <gregcosgrove@gmail.com>
2018-07-23 15:49:49 -0400

    Add portfolio position attribute

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,6 +11,10 @@ class Portfolio < ApplicationRecord
   #   where(subtitle: "Ruby on Rails")
   # end
 
+  def self.by_position
+    order("position ASC")
+  end
+
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
   scope :react_js, -> { where(subtitle: "React.js") }
   scope :node_js, -> { where(subtitle: "Node.js") }
