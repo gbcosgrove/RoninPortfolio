d2c3bb318a034f3a3360da07837cb2edf975e829
Greg Cosgrove <gregcosgrove@gmail.com>
2018-07-24 10:36:53 -0400

    Implement initial upload funcationality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -68,7 +68,12 @@ class PortfoliosController < ApplicationController
     end
 
     def portfolio_item_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle, 
+                                        :body,
+                                        :thumb_image,
+                                        :main_image,
+                                        technologies_attributes: [:name])
     end
 end
 
